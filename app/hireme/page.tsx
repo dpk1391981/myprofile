5bf92f77e147198d5b1f773fc27a795b4aa0f133
Deepak Kumar <deepak.kumar@instantsys.com>
2023-11-19 19:10:20 +0530

    add lloading

--- app/hireme/page.tsx
+++ app/hireme/page.tsx
@@ -6,6 +6,7 @@ import { IconArrowBackUp } from "@tabler/icons-react";
 
 const page = () => {
   let router = useRouter();
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     // Define your form fields here
     // For example:
@@ -24,6 +25,7 @@ const page = () => {
 
     try {
       // Make a POST request to your API route
+      setLoading(true);
       const response = await fetch("/api/hire/submit", {
         method: "POST",
         headers: {
@@ -36,6 +38,7 @@ const page = () => {
         const result = await response.json();
         console.log(result.data);
         if (result.data) {
+          setLoading(false);
           router.push("/success");
         }
       } else {
@@ -63,82 +66,95 @@ const page = () => {
             </a>
           </div>
         </div>
-        <form onSubmit={handleSubmit} className='space-y-8'>
-          <div>
-            <label aria-label='email' className='block mb-2 md:text-lg font-medium text-gray-900 dark:text-gray-300'>
-              Your email
-            </label>
-            <input
-              type='email'
-              name='email'
-              id='email'
-              className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 md:text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
-              placeholder='abc@abc.com'
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
+        {loading ? (
+          <div className='flex mr-15 text-center'>
+            <span className='loading loading-ball loading-xs text-blue-500'></span>
+            <span className='loading loading-ball loading-sm text-blue-500'></span>
+            <span className='loading loading-ball loading-md text-blue-500'></span>
+            <span className='loading loading-ball loading-lg text-blue-500'></span>
           </div>
-          <div>
-            <label
-              aria-label='oragnisation'
-              className='block mb-2 md:text-lg font-medium text-gray-900 dark:text-gray-300'>
-              Your organisation
-            </label>
-            <input
-              type='text'
-              id='organisation'
-              name='organisation'
-              className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 md:text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
-              placeholder='your organisation'
-              value={formData.organisation}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label aria-label='subject' className='block mb-2 md:text-lg font-medium text-gray-900 dark:text-gray-300'>
-              Subject
-            </label>
-            <input
-              type='text'
-              id='subject'
-              name='subject'
-              className='block p-3 w-full md:text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
-              placeholder='your subject'
-              value={formData.subject}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className='sm:col-span-2'>
-            <label aria-label='message' className='block mb-2 md:text-lg font-medium text-gray-900 dark:text-gray-400'>
-              Your message
-            </label>
-            <textarea
-              value={formData.message}
-              name='message'
-              onChange={handleChange}
-              id='message'
-              rows={5}
-              className='block p-2.5 w-full md:text-lg text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
-              placeholder='Please detail your requirement...'></textarea>
-          </div>
+        ) : (
+          <form onSubmit={handleSubmit} className='space-y-8'>
+            <div>
+              <label aria-label='email' className='block mb-2 md:text-lg font-medium text-gray-900 dark:text-gray-300'>
+                Your email
+              </label>
+              <input
+                type='email'
+                name='email'
+                id='email'
+                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 md:text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                placeholder='abc@abc.com'
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label
+                aria-label='oragnisation'
+                className='block mb-2 md:text-lg font-medium text-gray-900 dark:text-gray-300'>
+                Your organisation
+              </label>
+              <input
+                type='text'
+                id='organisation'
+                name='organisation'
+                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 md:text-lg rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                placeholder='your organisation'
+                value={formData.organisation}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label
+                aria-label='subject'
+                className='block mb-2 md:text-lg font-medium text-gray-900 dark:text-gray-300'>
+                Subject
+              </label>
+              <input
+                type='text'
+                id='subject'
+                name='subject'
+                className='block p-3 w-full md:text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light'
+                placeholder='your subject'
+                value={formData.subject}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className='sm:col-span-2'>
+              <label
+                aria-label='message'
+                className='block mb-2 md:text-lg font-medium text-gray-900 dark:text-gray-400'>
+                Your message
+              </label>
+              <textarea
+                value={formData.message}
+                name='message'
+                onChange={handleChange}
+                id='message'
+                rows={5}
+                className='block p-2.5 w-full md:text-lg text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+                placeholder='Please detail your requirement...'></textarea>
+            </div>
 
-          {/* <div className='sm:col-span-2'>
-            <input
-              type='file'
-              placeholder='Attach your requirement'
-              className=' text-blue-500 bg-blue-100 file-input file-input-bordered w-full max-w-xs'
-            />
-          </div> */}
+            {/* <div className='sm:col-span-2'>
+          <input
+            type='file'
+            placeholder='Attach your requirement'
+            className=' text-blue-500 bg-blue-100 file-input file-input-bordered w-full max-w-xs'
+          />
+        </div> */}
 
-          <button
-            type='submit'
-            className='px-5 md:text-lg font-medium text-center btn btn-outline btn-primary rounded-lg  sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>
-            Send query
-          </button>
-        </form>
+            <button
+              type='submit'
+              className='px-5 md:text-lg font-medium text-center btn btn-outline btn-primary rounded-lg  sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'>
+              Send query
+            </button>
+          </form>
+        )}
       </div>
     </section>
   );
